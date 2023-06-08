b919e5ada3a007ce5169ca636f6bc18bfd86294c
bitsgalore <johan.vanderknijff@kb.nl>
2023-06-08 21:44:09 +0200

    use Cramer's V to express association between JHOVE/VeraPDF output asnd rendering results

--- scripts/contingency-groundtruth.py
+++ scripts/contingency-groundtruth.py
@@ -2,6 +2,9 @@
 
 import argparse
 import pandas as pd
+import math
+import numpy as np
+from scipy import stats
 from tabulate import tabulate
 
 """
@@ -14,9 +17,46 @@ def dfToMarkdown(dataframe, headers='keys'):
     mdOut = dataframe.pipe(tabulate, headers=headers, tablefmt='pipe')
     return mdOut
 
+
+def cramersV(contTable):
+
+    """
+    Calculate Cramer's V from contingency table
+    Adapted from: https://towardsdatascience.com/correlation-when-pearsons-r-is-not-enough-aded72308635
+    """
+    #Calculate the chi-squared statistic and the p-value
+    chi2, p, dof, expected = stats.chi2_contingency(contTable)
+
+    #Calculate Cramer's V
+    V = math.sqrt(chi2 / (contTable.values.sum()*min(contTable.shape[0]-1, contTable.shape[1]-1)))
+
+    return V
+
+
+def cramersVCorr(var1, var2):
+
+    """ calculate Cramers V statistic for categorial-categorial association.
+    uses correction from Bergsma and Wicher, 
+    Journal of the Korean Statistical Society 42 (2013): 323-328
+    Adapted from:  https://stackoverflow.com/a/39266194/1209004
+    """
+    confusion_matrix = pd.crosstab(var1, var2)
+    chi2 = stats.chi2_contingency(confusion_matrix)[0]
+    #n = confusion_matrix.sum()
+    n = confusion_matrix.sum().sum()
+    phi2 = chi2/n
+    r,k = confusion_matrix.shape
+    phi2corr = max(0, phi2 - ((k-1)*(r-1))/(n-1))    
+    rcorr = r - ((r-1)**2)/(n-1)
+    kcorr = k - ((k-1)**2)/(n-1)
+    V = np.sqrt(phi2corr / min( (kcorr-1), (rcorr-1)))
+    return V
+
 def main():
     fileIn = "/home/johan/kb/pdf-risks/jhove-validation-errors/lindlar-tunnat-wilson-jhove-vera-rendering.csv"
     df = pd.read_csv(fileIn)
+
+    # Simple contingency tables
     contTabJHOVE = pd.crosstab(index=df['jhoveStatus'], columns=df['rendersInAcrobat'], margins=True)
     contTabVeraParseErrors = pd.crosstab(index=df['veraParseErrors'], columns=df['rendersInAcrobat'], margins=True)
     contTabVeraWarnings = pd.crosstab(index=df['veraLogWarnings'], columns=df['rendersInAcrobat'], margins=True)
@@ -31,6 +71,20 @@ def main():
         f.write(contTabVeraParseErrorsMd)
     with open("vera-warn-rendering.md", 'w') as f:
         f.write(contTabVeraWarningsMd)
+
+    # Calculate corrected Cramer's V as a measure of association between JHOVE/VeraPDF
+    # output and rendering results
+    # Note: since these are essentially ordinal data, more powerful measures such as
+    # Kendall Tau and Somers' D, but these don't work if one of the variables is
+    # dichotomic. 
+  
+    cVJhove = cramersVCorr(df['jhoveStatus'], df['rendersInAcrobat'])
+    cVVeraErrors = cramersVCorr(df['veraParseErrors'], df['rendersInAcrobat'])
+    cVVeraWarnings = cramersVCorr(df['veraLogWarnings'], df['rendersInAcrobat'])
+
+    print("Cramer's V (JHOVE): " + str(cVJhove))
+    print("Cramer's V (Vera Errors): " + str(cVVeraErrors))
+    print("Cramer's V (Vera Warnings): " + str(cVVeraWarnings))
 
 if __name__ == "__main__":
     main()
